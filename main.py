f77223cab84eb5044a98281fe34cc94e782c09f6
hammadmajid <hm.hammadmajid@gmail.com>
2022-10-16 20:03:23 +0500

    Code main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from json import load
+
+
+def main():
+    with open("random.json", "r") as file_in:
+        data = load(file_in)
+        length_of_list = len(data["random"])
+
+        for item_1 in range(length_of_list):
+            for item_2 in range(length_of_list):
+                if data["random"][item_1] < data["random"][item_2]:
+                    data["random"][item_1], data["random"][item_2] = swap(
+                        item_2, item_1
+                    )
+        for item in data["random"]:
+            print(item)
+
+
+def swap(x, y):
+    tmp = x
+    x = y
+    y = tmp
+    del tmp
+    return x, y
+
+
+if __name__ == "__main__":
+    main()
